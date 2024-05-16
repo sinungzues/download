9f3faacfc5e97ba9f7ad3a627357c87abafebf2c
Sinungzues <90892941+sinungzues@users.noreply.github.com>
2024-05-16 09:18:35 +0700

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -19,11 +19,11 @@ def download_image(url, directory):
         print(f"Error: Gagal mengunduh gambar {url}, kode status {response.status_code}")
 
 # URL dasar tempat gambar disimpan
-base_url = 'https://seantheme.com/color-admin/admin/assets/img/user/user-'
+base_url = 'https://blabla.com/assets/img/user/user-'
 # Direktori tempat menyimpan gambar
 download_dir = 'downloaded_images'
 
 # Loop untuk mengunduh gambar dari product-1.jpg sampai product-17.jpg
 for i in range(1, 18):
-    url = f"{base_url}{i}.jpg"
+    url = f"{base_url}{i}.jpg" #sesuaikan format file yang akan di download
     download_image(url, download_dir)
